664ee4ff2720cb4da7b1c4de22465c95c2ecc4c8
Rafael Cano <rafael.cano@ucsp.edu.pe>
2021-07-28 15:28:40 -0500

    Making cleaner to run every hour

--- src/cleaner.py
+++ src/cleaner.py
@@ -1,11 +1,16 @@
 from os import listdir, remove
 from os.path import isfile, join, getctime
-from time import time
-files = [f for f in listdir('./generador/pdfs/') if isfile(join('./generador/pdfs/', f))]
-curr_time = time()
-exceed_time = 3600 #one hour
-for file in files:
-    print(file)
-    filetime=getctime('./generador/pdfs/'+file)
-    if filetime+exceed_time<curr_time:
-        remove('./generador/pdfs/'+file)
+from time import time,sleep
+def limpiar():
+    files = [f for f in listdir('./generador/pdfs/') if isfile(join('./generador/pdfs/', f))]
+    curr_time = time()
+    exceed_time = 3600 #one hour
+    for file in files:
+        print(file)
+        filetime=getctime('./generador/pdfs/'+file)
+        if filetime+exceed_time<curr_time:
+            remove('./generador/pdfs/'+file)
+
+while True:
+    limpiar()
+    sleep(3600)#run every hour
